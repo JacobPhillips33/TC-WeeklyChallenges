7b3dffdf0db79bfcb873ebc633ab453c2ff6bf9b
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-23 20:51:31 -0500

    Completed Set 5

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,37 +8,98 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            int answer = 0;
+            for (int i = startNumber+1; i >= startNumber; i++)
+            {                
+                if (i % n == 0)
+                {
+                    answer = i;
+                    break;
+                }
+            }
+            return answer;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            foreach (var company in businesses)
+            {
+                if (company.TotalRevenue == 0)
+                {
+                    company.Name = "CLOSED";
+                }
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length < 1) return false;
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                if (numbers[i] > numbers[i+1])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) return 0;
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    for (int j = i+1; j < numbers.Length; j++)
+                    {
+                        sum += numbers[j];
+                        break;
+                    }
+                }
+            }
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            var joinedString = "";
+            if (words == null || words.Length < 1) return "";
+            foreach (var word in words)
+            {
+                joinedString += $" {word.Trim()}";
+                joinedString = joinedString.Trim();
+            }            
+            
+            if (joinedString.Length < 1) return "";
+            return $"{joinedString}.";
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
-        {
-            throw new NotImplementedException();
+        {            
+            if (elements == null) return Array.Empty<double>();
+            var forthElementList = new List<double>();
+            for (int i = 3; i < elements.Count; i += 4)
+            {
+                forthElementList.Add(elements[i]);
+            }
+            return forthElementList.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
