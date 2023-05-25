2a79a1a01bc8c9f4b9362ff77c79df38de0bedcf
Jake Phillips <jacob@nymblscience.com>
2023-05-25 09:16:54 -0600

    Created a branch to maintain blank challenges to start fresh from as desired.

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -10,52 +10,52 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            return Char.IsLetter(c);
+            throw new NotImplementedException();
         }
         
         public bool CountOfElementsIsEven(string[] vals)
         {
-            return vals.Length % 2 == 0;
+            throw new NotImplementedException();
         }
 
         public bool IsNumberEven(int number)
         {
-            return number % 2 == 0;
+            throw new NotImplementedException();
         }
 
         public bool IsNumberOdd(int num)
         {
-            return num % 2 != 0;
+            throw new NotImplementedException();
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            return numbers == null || numbers.Count() < 1 ? 0 : numbers.Min(x => x) + numbers.Max(x => x);
+            throw new NotImplementedException();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            return str1.Length <= str2.Length ? str1.Length : str2.Length;
+            throw new NotImplementedException();
         }
 
         public int Sum(int[] numbers)
         {
-            return numbers == null ? 0 : numbers.Sum(x => x);
+            throw new NotImplementedException();
         }
 
         public int SumEvens(int[] numbers)
         {
-            return numbers == null ? 0 : numbers.Sum(x => x % 2 == 0 ? x : 0);
+            throw new NotImplementedException();
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            return numbers == null ? false : numbers.Sum(x => x) % 2 != 0;
+            throw new NotImplementedException();
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            return number == null || number < 0 ? 0 : number / 2;
+            throw new NotImplementedException();
         }
     }
 }
